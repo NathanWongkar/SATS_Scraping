52e53251b62d106bf80dbbcad30d421c94a7d2eb
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-02-24 12:43:24 +0800

    added comment

--- Runner.py
+++ Runner.py
@@ -1,5 +1,5 @@
 from SatsScrape import SatsScrape, Day  
 
 if __name__ == "main":
+    # we need to check if it is weekday or weekend
     sats_scrape = SatsScrape("chromedriver", Day.WEEKDAY)
-    
